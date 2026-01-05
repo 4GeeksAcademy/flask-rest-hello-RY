909650aa7061b3760fb329e05c3d77587e3038f2
Rubeny94 <44436716+rubenyanes94@users.noreply.github.com>
2026-01-05 22:18:23 +0000

    [Instagram API simulation]

--- src/models.py
+++ src/models.py
@@ -4,16 +4,81 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
+    __tablename__ = "user"
     id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
+    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False, default=True)
 
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "is_active": self.is_active
         }
+
+
+class Post(db.Model):
+    __tablename__ = "post"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(nullable=False)
+    image_url: Mapped[str] = mapped_column(String(250), nullable=False)
+    caption: Mapped[str] = mapped_column(String(500), nullable=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "image_url": self.image_url,
+            "caption": self.caption
+        }
+
+
+class Comment(db.Model):
+    __tablename__ = "comment"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(nullable=False)
+    post_id: Mapped[int] = mapped_column(nullable=False)
+    text: Mapped[str] = mapped_column(String(500), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "post_id": self.post_id,
+            "text": self.text
+        }
+
+
+class Follower(db.Model):
+    __tablename__ = "follower"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_from_id: Mapped[int] = mapped_column(nullable=False)
+    user_to_id: Mapped[int] = mapped_column(nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_from_id": self.user_from_id,
+            "user_to_id": self.user_to_id
+        }
+
+
+class Like(db.Model):
+    __tablename__ = "like"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(nullable=False)
+    post_id: Mapped[int] = mapped_column(nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "post_id": self.post_id
+        }
+
+
